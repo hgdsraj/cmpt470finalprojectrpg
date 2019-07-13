9c97bda39f987a8d692fcc5f5010cf9e3e21ff2e
Adam Pruner <apruner@sfu.ca>
2019-07-12 21:31:21 -0700

    add some event handlers

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import {
+  Button,
   InputGroup,
   Input
 } from 'reactstrap';
 import "./Login.scss";
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    }
+  }
+
+  handleChangeUsername (event) {
+    console.log(event);
+  }
+
+  handleChangePassword (event) {
+    console.log(event);
+  }
+
   render () {
     return (
       <div className="login-page">
         <header className="login-header">
           <h1>Welcome to RPG470!</h1>
           <InputGroup className="login-input-group username">
-            <Input placeholder="Username"/>
+            <Input placeholder="Username" onChange={this.handleChangeUsername}/>
           </InputGroup>
           <InputGroup className="login-input-group password">
-            <Input placeholder="Password"/>
+            <Input placeholder="Password" onChange={this.handleChangePassword}/>
           </InputGroup>
+          <Button onClick={this.login}/>
         </header>
       </div>
     );
