6f3c018d6fb268252484ee6a301656f4ef4e74dc
Adam Pruner <apruner@sfu.ca>
2019-07-14 23:20:28 -0700

    begin writing the frontend test suite. Wrote basic login tests and they all pass, also created Signup test suite but haven't written tests yet

---
+++ frontend/src/Tests/Signup.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Signup from '../Components/Signup/Signup';
+
+describe('Signup component', () => {
+  it('renders without crashing', () => {
+    expect(true).toBe(true);
+  });
+});
