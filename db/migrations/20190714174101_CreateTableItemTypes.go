4247146bcf67cec1294baf36c38a7175a4b7695e
Vladimir Bereznyakov <vberezny@sfu.ca>
2019-07-14 17:49:53 -0700

    Made migration for creating table itemtypes

---
+++ db/migrations/20190714174101_CreateTableItemTypes.go
@@ -0,0 +1,27 @@
+
+package main
+
+import (
+	"database/sql"
+	"log"
+)
+
+// Up is executed when this migration is applied
+func Up_20190714174101(txn *sql.Tx) {
+	_, err := txn.Exec(	`CREATE TABLE IF NOT EXISTS ItemTypes(
+						 ID SERIAL primary key not null,
+						 TypeName text not null unique
+						)`)
+
+	if (err != nil) {
+		log.Fatalf("fatal error while running items migration %v", err)
+	}
+}
+
+// Down is executed when this migration is rolled back
+func Down_20190714174101(txn *sql.Tx) {
+	_, err := txn.Exec(`DROP TABLE ItemTypes`)
+	if (err != nil) {
+		log.Fatalf("fatal error while running items migration %v", err)
+	}
+}
