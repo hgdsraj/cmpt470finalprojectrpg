cf3a33c681f5ff38356bac1a1fefa5ee659bc03d
Adam Pruner <adampruner99@gmail.com>
2019-07-15 12:57:20 -0700

    write some tests for the Signup component

--- frontend/src/Tests/Login.test.js
+++ frontend/src/Tests/Login.test.js
@@ -5,35 +5,35 @@ import Login from '../Components/Login/Login';
 describe('Login component', () => {
 
   it('renders without crashing', () => {
-    const wrapper = shallow(<Login />)
+    const wrapper = shallow(<Login />);
     expect(wrapper.find('div.login-page').exists()).toBe(true);
     expect(wrapper.find('header.login-header').exists()).toBe(true);
   });
 
   it('renders two form groups', () => {
-    const wrapper = shallow(<Login />)
+    const wrapper = shallow(<Login />);
     expect(wrapper.find('.login-form-group').length).toEqual(2);
   });
 
   it('renders a label for username and password', () => {
-    const wrapper = shallow(<Login />)
+    const wrapper = shallow(<Login />);
     expect(wrapper.find('.login-form-label').length).toEqual(2);
   });
 
   it('renders a username input', () => {
-    const wrapper = shallow(<Login />)
+    const wrapper = shallow(<Login />);
     expect(wrapper.find('#username').length).toEqual(1);
   });
 
   it('renders a password input', () => {
-    const wrapper = shallow(<Login />)
+    const wrapper = shallow(<Login />);
     expect(wrapper.find('#password').length).toEqual(1);
   });
 
   // TODO: test username input checking
   describe('Username input', () => {
     it('responds to a change event by updating Login component state', () => {
-      const wrapper = shallow(<Login />)
+      const wrapper = shallow(<Login />);
       wrapper.find('#username').simulate('change', {target: {name: 'username', value: 'newusername'}});
       expect(wrapper.state('username')).toEqual('newusername');
     });
@@ -42,7 +42,7 @@ describe('Login component', () => {
   // TODO: test password input checking
   describe('Password input', () => {
     it('responds to a change event by updating Login component state', () => {
-      const wrapper = shallow(<Login />)
+      const wrapper = shallow(<Login />);
       wrapper.find('#password').simulate('change', {target: {name: 'password', value: 'newpassword'}});
       expect(wrapper.state('password')).toEqual('newpassword');
     });
--- frontend/src/Tests/Signup.test.js
+++ frontend/src/Tests/Signup.test.js
@@ -4,6 +4,58 @@ import Signup from '../Components/Signup/Signup';
 
 describe('Signup component', () => {
   it('renders without crashing', () => {
-    expect(true).toBe(true);
+    const wrapper = shallow(<Signup />);
+    expect(wrapper.find('.signup-page').exists()).toBe(true);
+  });
+
+  it('renders three form groups', () => {
+    const wrapper = shallow(<Signup />);
+    expect(wrapper.find('.signup-form-group').length).toEqual(3);
+  });
+
+  it('renders a label for username, password, and fullname', () => {
+    const wrapper = shallow(<Signup />);
+    expect(wrapper.find('.signup-form-label').length).toEqual(3);
+  });
+
+  it('renders a username input', () => {
+    const wrapper = shallow(<Signup />);
+    expect(wrapper.find('#username').length).toEqual(1);
+  });
+
+  it('renders a password input', () => {
+    const wrapper = shallow(<Signup />);
+    expect(wrapper.find('#password').length).toEqual(1);
+  });
+
+  it('renders a fullname input', () => {
+    const wrapper = shallow(<Signup />);
+    expect(wrapper.find('#fullname').length).toEqual(1);
+  });
+
+  // TODO: Test username input checking
+  describe('Username input', () => {
+    it('responds to a change event by updating Signup component state', () => {
+      const wrapper = shallow(<Signup />);
+      wrapper.find('#username').simulate('change', {target: {name: 'username', value: 'newusername'}});
+      expect(wrapper.state('username')).toEqual('newusername');
+    });
+  });
+
+  // TODO: Test password input checking
+  describe('Password input', () => {
+    it('responds to a change event by updating Signup component state', () => {
+      const wrapper = shallow(<Signup />);
+      wrapper.find('#password').simulate('change', {target: {name: 'password', value: 'newpassword'}});
+      expect(wrapper.state('password')).toEqual('newpassword');
+    });
+  });
+
+  describe('Fullname input', () => {
+    it('responds to a change event by updating Signup component state', () => {
+        const wrapper = shallow(<Signup />);
+        wrapper.find('#fullname').simulate('change', {target: {name: 'fullname', value: 'New Fullname'}});
+        expect(wrapper.state('fullname')).toEqual('New Fullname');
+    });
   });
 });
