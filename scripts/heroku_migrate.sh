5cc84c0238399d78bb54a76943fe99c0c6b00e48
hgdsraj <raj.axisos@gmail.com>
2019-07-15 22:45:17 -0700

    Altered heroku script to automatically grab the postgres URL from heroku config

--- scripts/heroku_migrate.sh
+++ scripts/heroku_migrate.sh
@@ -1,5 +1,8 @@
 cp db/dbconf.yml db/back.yml
+touch db/prod.yml
+printf "production:\n  driver: postgres\n  open: $(heroku config | grep DATABASE_URL: | sed -En "s/DATABASE_URL: //p")" > db/prod.yml
 cp db/prod.yml db/dbconf.yml
 goose -env production up
 cp db/back.yml db/dbconf.yml
 rm db/back.yml
+rm db/prod.yml
