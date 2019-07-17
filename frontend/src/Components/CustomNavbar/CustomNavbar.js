86639264aa1a16d4e19e3988fde2cc12050a20de
Adam Pruner <apruner@sfu.ca>
2019-07-17 14:31:20 -0700

    fix navbar proportions

--- frontend/src/Components/CustomNavbar/CustomNavbar.js
+++ frontend/src/Components/CustomNavbar/CustomNavbar.js
@@ -15,61 +15,63 @@ import Button from 'reactstrap/es/Button';
 class CustomNavbar extends React.Component {
   render () {
     return (
-      <div className="app-navbar">
-        <Navbar color="dark" expand="md">
-          <NavbarBrand className="nav-brand" href="/">RPG470</NavbarBrand>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink className="nav-link" href="/">
-                Home
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" href="/">
-                Battle
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle className="nav-link" nav caret>
-                Explore
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Map
-                </DropdownItem>
-                <DropdownItem>
-                  Shop
-                </DropdownItem>
-                <DropdownItem>
-                  Quests
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle className="nav-link" nav caret>
-                Character
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Overview
-                </DropdownItem>
-                <DropdownItem>
-                  Inventory
-                </DropdownItem>
-                <DropdownItem>
-                  Assign stats
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-            <NavItem>
-              <Button className="log-out-button" color="danger">
+      <div className="app-navbar-wrapper bg-dark">
+        <div className="app-navbar container">
+          <Navbar color="dark" expand="md">
+            <NavbarBrand className="nav-brand" href="/">RPG470</NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
                 <NavLink className="nav-link" href="/">
-                  Log out
+                  Home
                 </NavLink>
-              </Button>
-            </NavItem>
-          </Nav>
-        </Navbar>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" href="/">
+                  Battle
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle className="nav-link" nav caret>
+                  Explore
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Map
+                  </DropdownItem>
+                  <DropdownItem>
+                    Shop
+                  </DropdownItem>
+                  <DropdownItem>
+                    Quests
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle className="nav-link" nav caret>
+                  Character
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Overview
+                  </DropdownItem>
+                  <DropdownItem>
+                    Inventory
+                  </DropdownItem>
+                  <DropdownItem>
+                    Assign stats
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <Button className="log-out-button" color="danger">
+                  <NavLink className="nav-link" href="/">
+                    Log out
+                  </NavLink>
+                </Button>
+              </NavItem>
+            </Nav>
+          </Navbar>
+        </div>
       </div>
     )
   }
