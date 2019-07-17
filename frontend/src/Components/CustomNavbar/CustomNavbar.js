253d18bc325aacc44c334912fd26f804e402ea9e
Adam Pruner <apruner@sfu.ca>
2019-07-17 14:53:01 -0700

    Add and inject Navbar constants

--- frontend/src/Components/CustomNavbar/CustomNavbar.js
+++ frontend/src/Components/CustomNavbar/CustomNavbar.js
@@ -11,6 +11,9 @@ import {
   UncontrolledDropdown
 } from 'reactstrap';
 import Button from 'reactstrap/es/Button';
+import {
+  MSG_STRING_CONSTANTS
+} from '../../Constants/Constants';
 
 class CustomNavbar extends React.Component {
   render () {
@@ -18,54 +21,54 @@ class CustomNavbar extends React.Component {
       <div className="app-navbar-wrapper bg-dark">
         <div className="app-navbar container">
           <Navbar color="dark" expand="md">
-            <NavbarBrand className="nav-brand" href="/">RPG470</NavbarBrand>
+            <NavbarBrand className="nav-brand" href="/">{MSG_STRING_CONSTANTS.NAVBAR_NAV_BRAND_MSG}</NavbarBrand>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink className="nav-link" href="/">
-                  Home
+                <NavLink href="/">
+                  {MSG_STRING_CONSTANTS.NAVBAR_NAV_HOME_MSG}
                 </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink className="nav-link" href="/">
-                  Battle
+                <NavLink href="/">
+                  {MSG_STRING_CONSTANTS.NAVBAR_NAV_BATTLE_MSG}
                 </NavLink>
               </NavItem>
               <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle className="nav-link" nav caret>
-                  Explore
+                <DropdownToggle nav caret>
+                  {MSG_STRING_CONSTANTS.NAVBAR_NAV_EXPLORE_MSG}
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>
-                    Map
+                    {MSG_STRING_CONSTANTS.NAVBAR_NAV_EXPLORE_DROPDOWN_MAP_MSG}
                   </DropdownItem>
                   <DropdownItem>
-                    Shop
+                    {MSG_STRING_CONSTANTS.NAVBAR_NAV_EXPLORE_DROPDOWN_SHOP_MSG}
                   </DropdownItem>
                   <DropdownItem>
-                    Quests
+                    {MSG_STRING_CONSTANTS.NAVBAR_NAV_EXPLORE_DROPDOWN_QUESTS_MSG}
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
               <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle className="nav-link" nav caret>
-                  Character
+                <DropdownToggle nav caret>
+                  {MSG_STRING_CONSTANTS.NAVBAR_NAV_CHARACTER_MSG}
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>
-                    Overview
+                    {MSG_STRING_CONSTANTS.NAVBAR_NAV_CHARACTER_DROPDOWN_OVERVIEW_MSG}
                   </DropdownItem>
                   <DropdownItem>
-                    Inventory
+                    {MSG_STRING_CONSTANTS.NAVBAR_NAV_CHARACTER_DROPDOWN_INVENTORY_MSG}
                   </DropdownItem>
                   <DropdownItem>
-                    Assign stats
+                    {MSG_STRING_CONSTANTS.NAVBAR_NAV_CHARACTER_DROPDOWN_ASSIGNSTATS_MSG}
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
               <NavItem>
                 <Button className="log-out-button" color="danger">
-                  <NavLink className="nav-link" href="/">
-                    Log out
+                  <NavLink href="/">
+                    {MSG_STRING_CONSTANTS.NAVBAR_NAV_LOG_OUT_BUTTON_MSG}
                   </NavLink>
                 </Button>
               </NavItem>
