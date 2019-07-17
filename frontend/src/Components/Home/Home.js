d6df80366f0df36ad805ac04d06cdd2afb5e3840
Adam Pruner <apruner@sfu.ca>
2019-07-17 13:07:26 -0700

    mock up the battle content section

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  Button
+} from 'reactstrap';
 import './Home.scss';
 import CustomNavbar from "../CustomNavbar/CustomNavbar";
+import Goblin from "../../Assets/goblin.png";
+import Zombie from "../../Assets/zombie.png";
+import Imp from "../../Assets/imp.png";
 
 class Home extends React.Component {
   render () {
@@ -11,14 +22,40 @@ class Home extends React.Component {
           <div className="content home-page-centered-content container">
             <br/>
             <h2>Hello, *Character Name*!</h2>
-            <div className="mini-char-overview container">
+            <div className="mini-char-overview">
               <h5>Here there will be a mini character overview</h5>
             </div>
             <br/>
             <div className="battle-showcase container">
               <h3>Battle</h3>
-              <div className="battle-content container">
+              <div className="battle-content">
                 <h4>Battle the following NPCs to level up your character in combat</h4>
+                <div className="battle-npcs">
+                  <Card className="battle-npc-card">
+                    <CardImg className="battle-npc-cardimg" src={Goblin}/>
+                    <CardBody className="battle-npc-cardbody">
+                      <CardTitle className="battle-npc-title">NPC 1</CardTitle>
+                      <CardText className="battle-npc-text">Some text about this NPC</CardText>
+                      <Button color="primary" className="battle-npc-button">Fight</Button>
+                    </CardBody>
+                  </Card>
+                  <Card className="battle-npc-card">
+                    <CardImg className="battle-npc-cardimg" src={Zombie}/>
+                    <CardBody className="battle-npc-cardbody">
+                      <CardTitle className="battle-npc-title">NPC 2</CardTitle>
+                      <CardText className="battle-npc-text">Some text about this NPC</CardText>
+                      <Button color="primary" className="battle-npc-button">Fight</Button>
+                    </CardBody>
+                  </Card>
+                  <Card className="battle-npc-card">
+                    <CardImg className="battle-npc-cardimg" src={Imp}/>
+                    <CardBody className="battle-npc-cardbody">
+                      <CardTitle className="battle-npc-title">NPC 3</CardTitle>
+                      <CardText className="battle-npc-text">Some text about this NPC</CardText>
+                      <Button color="primary" className="battle-npc-button">Fight</Button>
+                    </CardBody>
+                  </Card>
+                </div>
               </div>
             </div>
             <br/>
