58f12c5319a31238c06fe98cb9f3f16f9c67ca2b
hgdsraj <raj.axisos@gmail.com>
2019-07-18 12:27:42 -0700

    added abilities to characters table (migration)

---
+++ db/migrations/20190718122322_AddAbilitiesToCharactersTable.go
@@ -0,0 +1,39 @@
+
+package main
+
+import (
+	"database/sql"
+	"log"
+)
+
+// Up is executed when this migration is applied
+func Up_20190718122322(txn *sql.Tx) {
+	
+	_, err := txn.Exec(`ALTER TABLE characters
+						ADD COLUMN stamina INT NOT NULL DEFAULT 0,
+						ADD COLUMN strength INT NOT NULL DEFAULT 0,
+						ADD COLUMN agility INT NOT NULL DEFAULT 0,
+						ADD COLUMN wisdom INT NOT NULL DEFAULT 0,
+						ADD COLUMN charisma INT NOT NULL DEFAULT 0
+						`)
+
+	if err != nil {
+		log.Fatalf("fatal error while running characters abilities migration %v", err)
+	}
+
+}
+
+// Down is executed when this migration is rolled back
+func Down_20190718122322(txn *sql.Tx) {
+	_, err := txn.Exec(`ALTER TABLE characters
+						DROP COLUMN stamina,
+						DROP COLUMN strength,
+						DROP COLUMN agility,
+						DROP COLUMN wisdom,
+						DROP COLUMN charisma
+						`)
+	if err != nil {
+		log.Fatalf("fatal error while running characters abilities migration %v", err)
+	}
+
+}
