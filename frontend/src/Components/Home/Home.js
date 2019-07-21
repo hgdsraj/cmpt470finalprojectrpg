3fcffb23ed7c0c46122f9d74c8105a3b4396265a
Adam Pruner <apruner@sfu.ca>
2019-07-21 14:00:59 -0700

    map mock data to jsx on the home page, add fields to mini char overview, fix some styling

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -22,23 +22,37 @@ import GrassMap from '../../Assets/grass_map.png';
 import BluePotion from '../../Assets/blue_potion.png';
 
 class Home extends React.Component {
-  // TODO: fetch character data and map to mini character display component
+
   renderMiniCharacterOverview = () => {
-    let characterName = 'character_name_here';
-    let characterAvatar = PrincessAvatar;
+    // TODO: fetch character data and map to mini character display component instead of using mock data
+    const mockCharacterData = {
+      name: 'Annabelle',
+      avatar: PrincessAvatar,
+      level: 1,
+      text: 'Here is some text about the character',
+      currentHealth: 25,
+      maxHealth: 25,
+      attack: 5,
+      defense: 4,
+      magicAttack: 4,
+      magicDefense: 3
+    };
+
     return (
       <div className="mini-char-overview showcase-container container">
-        <h2>{MSG_STRING_CONSTANTS.HOME_MINI_CHAR_OVERVIEW_HEADER_MSG + `${characterName}!`}</h2>
+        <h2>{MSG_STRING_CONSTANTS.HOME_MINI_CHAR_OVERVIEW_HEADER_MSG + `${mockCharacterData.characterName}!`}</h2>
         <div className="mini-char-overview-content">
           <Card className="mini-char-overview-card">
             <div className="mini-char-overview-wrapper">
-              <div className="mini-char-overview-intro overview-intro">
-                <CardImg className="mini-char-overview-cardimg cardimg" src={characterAvatar}/>
-                <CardBody className="mini-char-overview-cardbody cardbody">
-                  <CardTitle className="mini-char-overview-cardtitle cardtitle cardtext-color">{characterName}</CardTitle>
-                  <CardSubtitle className="mini-char-overview-cardsubtitle cardsubtitle">{MSG_STRING_CONSTANTS.LEVEL_MSG + "1"}</CardSubtitle>
-                  <CardText className="mini-char-overview-cardtext cardtext cardtext-color">Here is some text about the character</CardText>
-                </CardBody>
+              <div className="mini-char-overview-intro-flex-container overview-intro-flex-container">
+                <div className="mini-char-overview-intro overview-intro">
+                  <CardImg className="mini-char-overview-cardimg cardimg" src={mockCharacterData.avatar}/>
+                  <CardBody className="mini-char-overview-cardbody cardbody">
+                    <CardTitle className="mini-char-overview-cardtitle cardtitle cardtext-color">{mockCharacterData.characterName}</CardTitle>
+                    <CardSubtitle className="mini-char-overview-cardsubtitle cardsubtitle">{MSG_STRING_CONSTANTS.LEVEL_MSG + mockCharacterData.level.toString()}</CardSubtitle>
+                    <CardText className="mini-char-overview-cardtext cardtext cardtext-color">{mockCharacterData.text}</CardText>
+                  </CardBody>
+                </div>
               </div>
               <div className="mini-char-overview-stats overview-stats">
                 <Table>
@@ -51,15 +65,23 @@ class Home extends React.Component {
                   <tbody>
                   <tr>
                     <td>{MSG_STRING_CONSTANTS.HOME_MINI_CHAR_OVERVIEW_HEALTH_STAT_MSG}</td>
-                    <td>25/25</td>
+                    <td>{mockCharacterData.currentHealth}/{mockCharacterData.maxHealth}</td>
                   </tr>
                   <tr>
                     <td>{MSG_STRING_CONSTANTS.HOME_MINI_CHAR_OVERVIEW_ATTACK_STAT_MSG}</td>
-                    <td>5</td>
+                    <td>{mockCharacterData.attack}</td>
                   </tr>
                   <tr>
                     <td>{MSG_STRING_CONSTANTS.HOME_MINI_CHAR_OVERVIEW_DEFENSE_STAT_MSG}</td>
-                    <td>4</td>
+                    <td>{mockCharacterData.defense}</td>
+                  </tr>
+                  <tr>
+                    <td>{MSG_STRING_CONSTANTS.HOME_MINI_CHAR_OVERVIEW_MAGIC_ATTACK_STAT_MSG}</td>
+                    <td>{mockCharacterData.magicAttack}</td>
+                  </tr>
+                  <tr>
+                    <td>{MSG_STRING_CONSTANTS.HOME_MINI_CHAR_OVERVIEW_MAGIC_DEFENSE_STAT_MSG}</td>
+                    <td>{mockCharacterData.magicDefense}</td>
                   </tr>
                   </tbody>
                 </Table>
@@ -73,7 +95,43 @@ class Home extends React.Component {
   }
 
   renderBattleShowcase = () => {
-    // TODO: fetch NPCs to battle (near level) and map them to cards
+    // TODO: fetch NPCs to battle (near level) and map them to cards instead of using mock data
+    const mockNPCs = [
+      {
+        npcTitle: 'Goblin',
+        npcLevel: 1,
+        npcText: 'Some text about the goblin',
+        npcImgSrc: Goblin
+      },
+      {
+        npcTitle: 'Zombie',
+        npcLevel: 2,
+        npcText: 'Some text about the zombie',
+        npcImgSrc: Zombie
+      },
+      {
+        npcTitle: 'Imp',
+        npcLevel: 3,
+        npcText: 'Some text about the imp',
+        npcImgSrc: Imp
+      }
+    ];
+
+    const npcCards = mockNPCs.map((npcData, index) => {
+      return (
+        <Card key={index} className="battle-npc-card">
+          <CardImg className="battle-npc-cardimg cardimg" src={npcData.npcImgSrc}/>
+          <CardBody className="battle-npc-cardbody cardbody">
+            <div className="battle-npc-cardtitle-wrapper cardtitle-wrapper">
+              <CardTitle className="battle-npc-cardtitle cardtitle cardtext-color">{npcData.npcTitle}</CardTitle>
+              <CardSubtitle className="battle-npc-cardsubtitle cardsubtitle">{MSG_STRING_CONSTANTS.LEVEL_MSG + npcData.npcLevel.toString()}</CardSubtitle>
+            </div>
+            <CardText className="battle-npc-cardtext cardtext cardtext-color">{npcData.npcText}</CardText>
+            <Button color="primary" className="battle-npc-button cardbutton">{MSG_STRING_CONSTANTS.HOME_BATTLE_SHOWCASE_CARD_BUTTON_MSG}</Button>
+          </CardBody>
+        </Card>
+      );
+    });
 
     return (
       <div className="battle-showcase showcase-container container">
@@ -81,39 +139,7 @@ class Home extends React.Component {
         <div className="battle-content">
           <h4>{MSG_STRING_CONSTANTS.HOME_BATTLE_SHOWCASE_SUBTITLE_MSG}</h4>
           <div className="battle-npcs card-container">
-            <Card className="battle-npc-card">
-              <CardImg className="battle-npc-cardimg cardimg" src={Goblin}/>
-              <CardBody className="battle-npc-cardbody cardbody">
-                <div className="battle-npc-cardtitle-wrapper cardtitle-wrapper">
-                  <CardTitle className="battle-npc-cardtitle cardtitle cardtext-color">Goblin</CardTitle>
-                  <CardSubtitle className="battle-npc-cardsubtitle cardsubtitle">{MSG_STRING_CONSTANTS.LEVEL_MSG + "1"}</CardSubtitle>
-                </div>
-                <CardText className="battle-npc-cardtext cardtext cardtext-color">Some text about the goblin</CardText>
-                <Button color="primary" className="battle-npc-button cardbutton">{MSG_STRING_CONSTANTS.HOME_BATTLE_SHOWCASE_CARD_BUTTON_MSG}</Button>
-              </CardBody>
-            </Card>
-            <Card className="battle-npc-card">
-              <CardImg className="battle-npc-cardimg cardimg" src={Zombie}/>
-              <CardBody className="battle-npc-cardbody cardbody">
-                <div className="battle-npc-cardtitle-wrapper cardtitle-wrapper">
-                  <CardTitle className="battle-npc-cardtitle cardtitle cardtext-color">Zombie</CardTitle>
-                  <CardSubtitle className="battle-npc-cardsubtitle cardsubtitle">{MSG_STRING_CONSTANTS.LEVEL_MSG + "2"}</CardSubtitle>
-                </div>
-                <CardText className="battle-npc-cardtext cardtext cardtext-color">Some text about the zombie</CardText>
-                <Button color="primary" className="battle-npc-button cardbutton">{MSG_STRING_CONSTANTS.HOME_BATTLE_SHOWCASE_CARD_BUTTON_MSG}</Button>
-              </CardBody>
-            </Card>
-            <Card className="battle-npc-card">
-              <CardImg className="battle-npc-cardimg cardimg" src={Imp}/>
-              <CardBody className="battle-npc-cardbody cardbody">
-                <div className="battle-npc-cardtitle-wrapper cardtitle-wrapper">
-                  <CardTitle className="battle-npc-cardtitle cardtitle cardtext-color">Imp</CardTitle>
-                  <CardSubtitle className="battle-npc-cardsubtitle cardsubtitle">{MSG_STRING_CONSTANTS.LEVEL_MSG + "3"}</CardSubtitle>
-                </div>
-                <CardText className="battle-npc-text cardtext cardtext-color">Some text about the imp</CardText>
-                <Button color="primary" className="battle-npc-button cardbutton">{MSG_STRING_CONSTANTS.HOME_BATTLE_SHOWCASE_CARD_BUTTON_MSG}</Button>
-              </CardBody>
-            </Card>
+            {npcCards}
           </div>
         </div>
       </div>
@@ -121,8 +147,81 @@ class Home extends React.Component {
   }
 
   renderExploreShowcase = () => {
-    // TODO: Fetch explore maps and map them to map cards
-    // TODO: Fetch explore shop items and map them to shop item cards
+    // TODO: Fetch explore maps and map them to map cards instead of using mock data
+    const mockMaps = [
+      {
+        mapTitle: 'Grasslands',
+        mapText: 'Some text about this map',
+        mapImgSrc: GrassMap
+      },
+      {
+        mapTitle: 'Grasslands',
+        mapText: 'Some text about this map',
+        mapImgSrc: GrassMap
+      },
+      {
+        mapTitle: 'Grasslands',
+        mapText: 'Some text about this map',
+        mapImgSrc: GrassMap
+      }
+    ];
+
+    const exploreMapCards = mockMaps.map((mapData, index) => {
+      return (
+        <Card key={index} className="explore-map-card">
+          <CardImg className="explore-map-cardimg cardimg" src={mapData.mapImgSrc}/>
+          <CardBody className="explore-map-cardbody cardbody">
+            <CardTitle className="explore-map-title cardtitle cardtext-color">{mapData.mapTitle}</CardTitle>
+            <CardText className="explore-map-text cardtext cardtext-color">{mapData.mapText}</CardText>
+            <Button color="primary" className="explore-map-button cardbutton">{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_MAP_CARD_BUTTON_MSG}</Button>
+          </CardBody>
+        </Card>
+      );
+    });
+
+    // TODO: Fetch explore shop items and map them to shop item cards instead of using mock data
+    const mockShopItems = [
+      {
+        shopItemTitle: 'Potion',
+        shopItemSubtitle: 'Costs 5 gold',
+        shopItemText: 'Some text about this item',
+        shopItemImgSrc: BluePotion
+      },
+      {
+        shopItemTitle: 'Potion',
+        shopItemSubtitle: 'Costs 5 gold',
+        shopItemText: 'Some text about this item',
+        shopItemImgSrc: BluePotion
+      },
+      {
+        shopItemTitle: 'Potion',
+        shopItemSubtitle: 'Costs 5 gold',
+        shopItemText: 'Some text about this item',
+        shopItemImgSrc: BluePotion
+      },
+      {
+        shopItemTitle: 'Potion',
+        shopItemSubtitle: 'Costs 5 gold',
+        shopItemText: 'Some text about this item',
+        shopItemImgSrc: BluePotion
+      }
+    ];
+
+    const exploreShopItemCards = mockShopItems.map((shopItemData, index) => {
+      return (
+        <Card key={index} className="explore-shop-card">
+          <CardImg className="explore-shop-cardimg cardimg" src={shopItemData.shopItemImgSrc}/>
+          <CardBody className="explore-shop-cardbody cardbody">
+            <div className="explore-shop-cardtitle-wrapper cardtitle-wrapper">
+              <CardTitle className="explore-shop-cardtitle cardtitle cardtext-color">{shopItemData.shopItemTitle}</CardTitle>
+              <CardSubtitle className="explore-shop-cardsubtitle cardsubtitle">{shopItemData.shopItemSubtitle}</CardSubtitle>
+            </div>
+            <CardText className="explore-shop-text cardtext cardtext-color">{shopItemData.shopItemText}</CardText>
+            <Button color="primary" className="explore-shop-button cardbutton">{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_SHOP_CARD_BUTTON_MSG}</Button>
+          </CardBody>
+        </Card>
+      );
+    });
 
     return (
       <div className="explore-showcase showcase-container container">
@@ -130,77 +229,11 @@ class Home extends React.Component {
         <div className="explore-content">
           <h4>{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_MAP_SUBTITLE_MSG}</h4>
           <div className="explore-maps card-container">
-            <Card className="explore-map-card">
-              <CardImg className="explore-map-cardimg cardimg" src={GrassMap}/>
-              <CardBody className="explore-map-cardbody cardbody">
-                <CardTitle className="explore-map-title cardtitle cardtext-color">Map</CardTitle>
-                <CardText className="explore-map-text cardtext cardtext-color">Some text about this map</CardText>
-                <Button color="primary" className="explore-map-button cardbutton">{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_MAP_CARD_BUTTON_MSG}</Button>
-              </CardBody>
-            </Card>
-            <Card className="explore-map-card">
-              <CardImg className="explore-map-cardimg cardimg" src={GrassMap}/>
-              <CardBody className="explore-map-cardbody cardbody">
-                <CardTitle className="explore-map-title cardtitle cardtext-color">Map</CardTitle>
-                <CardText className="explore-map-text cardtext cardtext-color">Some text about this map</CardText>
-                <Button color="primary" className="explore-map-button cardbutton">{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_MAP_CARD_BUTTON_MSG}</Button>
-              </CardBody>
-            </Card>
-            <Card className="explore-map-card">
-              <CardImg className="explore-map-cardimg cardimg" src={GrassMap}/>
-              <CardBody className="explore-map-cardbody cardbody">
-                <CardTitle className="explore-map-title cardtitle cardtext-color">Map</CardTitle>
-                <CardText className="explore-map-text cardtext cardtext-color">Some text about this map</CardText>
-                <Button color="primary" className="explore-map-button cardbutton">{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_MAP_CARD_BUTTON_MSG}</Button>
-              </CardBody>
-            </Card>
+            {exploreMapCards}
           </div>
           <h4>{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_SHOP_SUBTITLE_MSG}</h4>
           <div className="explore-shop card-container">
-            <Card className="explore-shop-card">
-              <CardImg className="explore-shop-cardimg cardimg" src={BluePotion}/>
-              <CardBody className="explore-shop-cardbody cardbody">
-                <div className="explore-shop-cardtitle-wrapper cardtitle-wrapper">
-                  <CardTitle className="explore-shop-cardtitle cardtitle cardtext-color">Potion</CardTitle>
-                  <CardSubtitle className="explore-shop-cardsubtitle cardsubtitle">Costs 5 gold</CardSubtitle>
-                </div>
-                <CardText className="explore-shop-text cardtext cardtext-color">Some text about this item</CardText>
-                <Button color="primary" className="explore-shop-button cardbutton">{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_SHOP_CARD_BUTTON_MSG}</Button>
-              </CardBody>
-            </Card>
-            <Card className="explore-shop-card">
-              <CardImg className="explore-shop-cardimg cardimg" src={BluePotion}/>
-              <CardBody className="explore-shop-cardbody cardbody">
-                <div className="explore-shop-cardtitle-wrapper cardtitle-wrapper">
-                  <CardTitle className="explore-shop-cardtitle cardtitle cardtext-color">Potion</CardTitle>
-                  <CardSubtitle className="explore-shop-cardsubtitle cardsubtitle">Costs 5 gold</CardSubtitle>
-                </div>
-                <CardText className="explore-shop-text cardtext cardtext-color">Some text about this item</CardText>
-                <Button color="primary" className="explore-shop-button cardbutton">{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_SHOP_CARD_BUTTON_MSG}</Button>
-              </CardBody>
-            </Card>
-            <Card className="explore-shop-card">
-              <CardImg className="explore-shop-cardimg cardimg" src={BluePotion}/>
-              <CardBody className="explore-shop-cardbody cardbody">
-                <div className="explore-shop-cardtitle-wrapper cardtitle-wrapper">
-                  <CardTitle className="explore-shop-cardtitle cardtitle cardtext-color">Potion</CardTitle>
-                  <CardSubtitle className="explore-shop-cardsubtitle cardsubtitle">Costs 5 gold</CardSubtitle>
-                </div>
-                <CardText className="explore-shop-text cardtext cardtext-color">Some text about this item</CardText>
-                <Button color="primary" className="explore-shop-button cardbutton">{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_SHOP_CARD_BUTTON_MSG}</Button>
-              </CardBody>
-            </Card>
-            <Card className="explore-shop-card">
-              <CardImg className="explore-shop-cardimg cardimg" src={BluePotion}/>
-              <CardBody className="explore-shop-cardbody cardbody">
-                <div className="explore-shop-cardtitle-wrapper cardtitle-wrapper">
-                  <CardTitle className="explore-shop-cardtitle cardtitle cardtext-color">Potion</CardTitle>
-                  <CardSubtitle className="explore-shop-cardsubtitle cardsubtitle">Costs 5 gold</CardSubtitle>
-                </div>
-                <CardText className="explore-shop-text cardtext cardtext-color">Some text about this item</CardText>
-                <Button color="primary" className="explore-shop-button cardbutton">{MSG_STRING_CONSTANTS.HOME_EXPLORE_SHOWCASE_SHOP_CARD_BUTTON_MSG}</Button>
-              </CardBody>
-            </Card>
+            {exploreShopItemCards}
           </div>
         </div>
       </div>
