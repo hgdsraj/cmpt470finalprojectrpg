b40373e0118514297360feffd5b17a7b9d689e9b
Vladimir Bereznyakov <vberezny@sfu.ca>
2019-07-26 16:44:39 -0700

    Created mockup of character and npc cards side by side on battle page

--- frontend/src/Components/Battle/Battle.js
+++ frontend/src/Components/Battle/Battle.js
@@ -8,7 +8,7 @@ import {
   Label,
   Card,
   CardImg,
-  Table
+  Table, CardBody, CardTitle, CardSubtitle, CardText
 } from 'reactstrap';
 import {
   NUMBERS,
@@ -20,6 +20,9 @@ import {
 import CustomNavbar from '../CustomNavbar/CustomNavbar';
 import './Battle.scss';
 import PrincessAvatar from '../../Assets/princess_avatar.png';
+import Goblin from "../../Assets/goblin.png";
+import Zombie from "../../Assets/zombie.png";
+import Imp from "../../Assets/imp.png";
 
 class Battle extends React.Component {
   // constructor(props) {
@@ -64,40 +67,149 @@ class Battle extends React.Component {
   // }
   //
   render() {
+    const mockNPCs = [
+      {
+        npcTitle: 'Goblin',
+        npcLevel: 1,
+        npcText: 'Some text about the goblin',
+        npcImgSrc: Goblin
+      },
+      {
+        npcTitle: 'Zombie',
+        npcLevel: 2,
+        npcText: 'Some text about the zombie',
+        npcImgSrc: Zombie
+      },
+      {
+        npcTitle: 'Imp',
+        npcLevel: 3,
+        npcText: 'Some text about the imp',
+        npcImgSrc: Imp
+      }
+    ];
+
+    const mockCharacterData = {
+      name: 'Annabelle',
+      avatar: PrincessAvatar,
+      level: 1,
+      text: 'Here is some text about the character',
+      currentHealth: 25,
+      maxHealth: 25,
+      attack: 5,
+      defense: 4,
+      magicAttack: 4,
+      magicDefense: 3
+    };
+
     return (
       <div className="battle-page page-container">
         <CustomNavbar/>
          {/*TODO: Change CSS such that we don't need this full-viewport-with-navbar class - use flexbox page-containers instead*/}
         <div className="battle-centered-content full-viewport-with-navbar centered content container">
           <div className="battle-viewport-width">
-            <h1 className="battle-header-text">{STRINGS.CREATE_CHARACTER_HEADER_MSG}</h1>
-            {/*<Form onSubmit={this.handleCreateCharacter}>*/}
-            {/*  <FormGroup className="battle-form-group">*/}
-            {/*    <Label for="charactername" className="battle-form-label form-label">*/}
-            {/*      {STRINGS.CREATE_CHARACTER_CHARACTER_NAME_MSG}*/}
-            {/*    </Label>*/}
-            {/*    <Input type="charactername" id="charactername"*/}
-            {/*           onChange={this.handleChangeCharacterName}/>*/}
-            {/*  </FormGroup>*/}
-            {/*  <AvatarSelect*/}
-            {/*    avatars={STRINGS.CREATE_CHARACTER_AVATAR_NAMES}*/}
-            {/*    onChange={this.handleChangeAvatarSelection}*/}
-            {/*  />*/}
-            {/*  <AssignStatsTable*/}
-            {/*    stats={this.state.stats}*/}
-            {/*    remainingStatPoints={this.state.remainingStatPoints}*/}
-            {/*    statIds={STRINGS.CREATE_CHARACTER_STAT_IDS}*/}
-            {/*    buttonOnClick={this.handleAddOrSubtractStat}*/}
-            {/*  />*/}
-            {/*  <Button*/}
-            {/*    color="primary"*/}
-            {/*    id="create"*/}
-            {/*    className="create-button"*/}
-            {/*    disabled={this.state.remainingStatPoints > 0 || !this.state.characterName || !this.state.avatarSelection}*/}
-            {/*  >*/}
-            {/*    {STRINGS.CREATE_CHARACTER_CREATE_BUTTON_MSG}*/}
-            {/*  </Button>*/}
-            {/*</Form>*/}
+            <h1 className="battle-header-text">{STRINGS.BATTLE_HEADER_MSG}</h1>
+            <div className="battle-container container">
+              {/*TODO: maybe make this below the cards*/}
+              <h3 className="battle-container-header-text">{STRINGS.BATTLE_CONTAINER_HEADER_MSG}</h3>
+              <div className="battle-card-container container">
+                <Card className="battle-character-card">
+                  <div className="char-overview-wrapper">
+                    <div className="char-overview-intro-flex-container overview-intro-flex-container">
+                      <div className="char-overview-intro overview-intro">
+                        <CardImg className="char-overview-cardimg cardimg"
+                                 src={mockCharacterData.avatar}/>
+                        <CardBody className="char-overview-cardbody cardbody">
+                          <CardTitle className="char-overview-cardtitle cardtitle cardtext-color">{mockCharacterData.name}</CardTitle>
+                          <CardSubtitle className="char-overview-cardsubtitle cardsubtitle">{STRINGS.HOME_LEVEL_MSG + mockCharacterData.level.toString()}</CardSubtitle>
+                          <CardText className="char-overview-cardtext cardtext cardtext-color">{mockCharacterData.text}</CardText>
+                        </CardBody>
+                      </div>
+                    </div>
+                    <div className="char-overview-stats overview-stats">
+                      <Table>
+                        <thead>
+                        <tr>
+                          <th>{STRINGS.BATTLE_STAT_MSG}</th>
+                          <th>{STRINGS.BATTLE_VALUE_MSG}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                          <td>{STRINGS.BATTLE_HEALTH_STAT_MSG}</td>
+                          <td>{mockCharacterData.currentHealth}/{mockCharacterData.maxHealth}</td>
+                        </tr>
+                        <tr>
+                          <td>{STRINGS.BATTLE_ATTACK_STAT_MSG}</td>
+                          <td>{mockCharacterData.attack}</td>
+                        </tr>
+                        <tr>
+                          <td>{STRINGS.BATTLE_DEFENSE_STAT_MSG}</td>
+                          <td>{mockCharacterData.defense}</td>
+                        </tr>
+                        <tr>
+                          <td>{STRINGS.BATTLE_MAGIC_ATTACK_STAT_MSG}</td>
+                          <td>{mockCharacterData.magicAttack}</td>
+                        </tr>
+                        <tr>
+                          <td>{STRINGS.BATTLE_MAGIC_DEFENSE_STAT_MSG}</td>
+                          <td>{mockCharacterData.magicDefense}</td>
+                        </tr>
+                        </tbody>
+                      </Table>
+                    </div>
+                    <div className="overview-clear"/>
+                  </div>
+                </Card>
+                <Card className="battle-npc-card">
+                  <div className="char-overview-wrapper">
+                    <div className="char-overview-intro-flex-container overview-intro-flex-container">
+                      <div className="char-overview-intro overview-intro">
+                        <CardImg className="char-overview-cardimg cardimg"
+                                 src={mockCharacterData.avatar}/>
+                        <CardBody className="char-overview-cardbody cardbody">
+                          <CardTitle className="char-overview-cardtitle cardtitle cardtext-color">{mockCharacterData.name}</CardTitle>
+                          <CardSubtitle className="char-overview-cardsubtitle cardsubtitle">{STRINGS.HOME_LEVEL_MSG + mockCharacterData.level.toString()}</CardSubtitle>
+                          <CardText className="char-overview-cardtext cardtext cardtext-color">{mockCharacterData.text}</CardText>
+                        </CardBody>
+                      </div>
+                    </div>
+                    <div className="char-overview-stats overview-stats">
+                      <Table>
+                        <thead>
+                        <tr>
+                          <th>{STRINGS.BATTLE_STAT_MSG}</th>
+                          <th>{STRINGS.BATTLE_VALUE_MSG}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                          <td>{STRINGS.BATTLE_HEALTH_STAT_MSG}</td>
+                          <td>{mockCharacterData.currentHealth}/{mockCharacterData.maxHealth}</td>
+                        </tr>
+                        <tr>
+                          <td>{STRINGS.BATTLE_ATTACK_STAT_MSG}</td>
+                          <td>{mockCharacterData.attack}</td>
+                        </tr>
+                        <tr>
+                          <td>{STRINGS.BATTLE_DEFENSE_STAT_MSG}</td>
+                          <td>{mockCharacterData.defense}</td>
+                        </tr>
+                        <tr>
+                          <td>{STRINGS.BATTLE_MAGIC_ATTACK_STAT_MSG}</td>
+                          <td>{mockCharacterData.magicAttack}</td>
+                        </tr>
+                        <tr>
+                          <td>{STRINGS.BATTLE_MAGIC_DEFENSE_STAT_MSG}</td>
+                          <td>{mockCharacterData.magicDefense}</td>
+                        </tr>
+                        </tbody>
+                      </Table>
+                    </div>
+                    <div className="overview-clear"/>
+                  </div>
+                </Card>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- frontend/src/Constants/BattleConstants.js
+++ frontend/src/Constants/BattleConstants.js
@@ -1,19 +1,19 @@
 // const NUMBERS = {
-//   CREATE_CHARACTER_STAT_INDEX_MAP: {
+//   BATTLE_STAT_INDEX_MAP: {
 //     'stamina': 0,
 //     'strength': 1,
 //     'agility': 2,
 //     'wisdom': 3,
 //     'charisma': 4
 //   },
-//   CREATE_CHARACTER_MIN_STAT_VALUES: [
+//   BATTLE_MIN_STAT_VALUES: [
 //     12,
 //     10,
 //     10,
 //     11,
 //     11
 //   ],
-//   CREATE_CHARACTER_MAX_STAT_VALUES: [
+//   BATTLE_MAX_STAT_VALUES: [
 //     16,
 //     14,
 //     14,
@@ -23,53 +23,16 @@
 // };
 
 const STRINGS = {
-  CREATE_CHARACTER_HEADER_MSG: 'Create a character',
-  CREATE_CHARACTER_CHARACTER_NAME_MSG: 'Character name',
-  CREATE_CHARACTER_ADD_STAT_MSG: 'add',
-  CREATE_CHARACTER_SUBTRACT_STAT_MSG: 'subtract',
-  CREATE_CHARACTER_STAT_STAM_MSG: 'stamina',
-  CREATE_CHARACTER_STAT_STR_MSG: 'strength',
-  CREATE_CHARACTER_STAT_AGI_MSG: 'agility',
-  CREATE_CHARACTER_STAT_WIS_MSG: 'wisdom',
-  CREATE_CHARACTER_STAT_CHAR_MSG: 'charisma',
-  CREATE_CHARACTER_AVATAR_SELECT_AVATAR_MSG: 'Avatar',
-  CREATE_CHARACTER_AVATAR_NAMES: [
-    'Princess',
-    'Vampire',
-    'Knight',
-    'Warrior',
-    'Cleric',
-    'Hunter'
-  ],
-  CREATE_CHARACTER_STAT_IDS: [
-    [
-      'subtract-stamina',
-      'add-stamina'
-    ],
-    [
-      'subtract-strength',
-      'add-strength'
-    ],
-    [
-      'subtract-agility',
-      'add-agility'
-    ],
-    [
-      'subtract-wisdom',
-      'add-wisdom'
-    ],
-    [
-      'subtract-charisma',
-      'add-charisma'
-    ],
-  ],
-  CREATE_CHARACTER_ADD_STAT_BUTTON_MSG: '+',
-  CREATE_CHARACTER_SUBTRACT_STAT_BUTTON_MSG: '-',
-  CREATE_CHARACTER_CREATE_BUTTON_MSG: 'Create',
-  CREATE_CHARACTER_ASSIGN_STATS_POINTS_MSG_ROOT: 'Assign stats (You have ',
-  CREATE_CHARACTER_ASSIGN_STATS_POINTS_MSG_END: ' points left to assign)',
-  CREATE_CHARACTER_ASSIGN_STATS_STAT_MSG: 'Stat',
-  CREATE_CHARACTER_ASSIGN_STATS_VALUE_MSG: 'Value'
+  BATTLE_HEADER_MSG: 'You are challenged to a battle!',
+  BATTLE_CHARACTER_NAME_MSG: 'Character name',
+  BATTLE_CONTAINER_HEADER_MSG: 'Place Holder Battle Text',
+  BATTLE_STAT_MSG: 'Stat',
+  BATTLE_VALUE_MSG: 'Value',
+  BATTLE_HEALTH_STAT_MSG: 'Health',
+  BATTLE_ATTACK_STAT_MSG: 'Attack',
+  BATTLE_DEFENSE_STAT_MSG: 'Defense',
+  BATTLE_MAGIC_ATTACK_STAT_MSG: 'Magic Attack',
+  BATTLE_MAGIC_DEFENSE_STAT_MSG: 'Magic Defense',
 };
 
 export {
